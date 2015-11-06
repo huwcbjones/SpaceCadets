ff92b68d02ee18d688a377fe0829ee4a02d17639
Huw Jones <huwcbjones@gmail.com>
2015-11-06 23:01:43 +0000

    Added TargetNotFoundException for when message targets are not found

---
+++ Challenge 6/src/com/huwcbjones/chat/core/exceptions/TargetNotFoundException.java
@@ -0,0 +1,10 @@
+package com.huwcbjones.chat.core.exceptions;
+
+/**
+ * Thrown when the target of a message could not be found
+ *
+ * @author Huw Jones
+ * @since 06/11/2015
+ */
+public class TargetNotFoundException extends Exception {
+}
