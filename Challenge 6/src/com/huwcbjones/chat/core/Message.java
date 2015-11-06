7f6379f3e284e4d463a10ec3804b230c017b4a02
Huw Jones <huwcbjones@gmail.com>
2015-11-06 23:02:30 +0000

    Added message target to Message class

--- Challenge 6/src/com/huwcbjones/chat/core/Message.java
+++ Challenge 6/src/com/huwcbjones/chat/core/Message.java
@@ -14,8 +14,9 @@ public class Message {
     private User _user;
     private String _message;
     private Date _timestamp;
+    private Target _target;
 
-    public Message(User user, String message){
+    public Message(User user, String message) {
         this._user = user;
         this._message = message;
         this._timestamp = new Date();
@@ -23,17 +24,36 @@ public class Message {
 
     /**
      * Gets the message
+     *
      * @return String
      */
-    public String getMessage(){
+    public String getMessage() {
         return this._message;
     }
 
     /**
      * Gets user that sent the message
+     *
      * @return User
      */
-    public User getUser(){
+    public User getUser() {
         return this._user;
     }
+
+    /**
+     * Gets the time the message was processed by the server
+     * @return Timestamp
+     */
+    public Date getTimestamp() {
+        return this._timestamp;
+    }
+
+    /**
+     * Gets message target
+     * @return Target
+     */
+    public Target getTarget() {
+        return this._target;
+    }
+
 }
