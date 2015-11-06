8f84f57f8b838bea0c0b1e8b2428c11cbbd5440b
Huw Jones <huwcbjones@gmail.com>
2015-11-06 23:33:07 +0000

    Added Client/Message failure exceptions

---
+++ Challenge 6/src/com/huwcbjones/chat/core/exceptions/ClientNotConnectedException.java
@@ -0,0 +1,10 @@
+package com.huwcbjones.chat.core.exceptions;
+
+/**
+ * {DESCRIPTION}
+ *
+ * @author Huw Jones
+ * @since 06/11/2015
+ */
+public class ClientNotConnectedException extends Exception {
+}
---
+++ Challenge 6/src/com/huwcbjones/chat/core/exceptions/MessageSendFailException.java
@@ -0,0 +1,10 @@
+package com.huwcbjones.chat.core.exceptions;
+
+/**
+ * Thrown when a message failed to send
+ *
+ * @author Huw Jones
+ * @since 06/11/2015
+ */
+public class MessageSendFailException extends Exception {
+}
