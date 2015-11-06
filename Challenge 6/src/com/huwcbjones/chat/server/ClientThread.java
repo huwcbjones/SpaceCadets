576fa71635e81d6153815c176b64d4e85efdd820
Huw Jones <huwcbjones@gmail.com>
2015-11-06 23:35:39 +0000

    Added send message methodology for ClientThread

--- Challenge 6/src/com/huwcbjones/chat/server/ClientThread.java
+++ Challenge 6/src/com/huwcbjones/chat/server/ClientThread.java
@@ -1,25 +1,81 @@
 package com.huwcbjones.chat.server;
 
+import com.huwcbjones.chat.core.Message;
+import com.huwcbjones.chat.core.exceptions.ClientNotConnectedException;
+import com.sun.deploy.util.SessionState;
+import com.sun.security.ntlm.Client;
+
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Handles Clients on the Server
  *
  * @author Huw Jones
  * @since 06/11/2015
  */
-public class ClientThread implements Runnable {
+public class ClientThread extends Thread {
+
+    private Socket _socket;
+    private BufferedReader _input;
+    private ObjectOutputStream _output;
+    private Server _server;
+    private int _clientID = this.hashCode();
+
+    private boolean _isConnected = false;
+
+    public ClientThread(Server server, Socket socket) {
+        this._server = server;
+        this._socket = socket;
+    }
+
     /**
-     * When an object implementing interface <code>Runnable</code> is used
-     * to create a thread, starting the thread causes the object's
-     * <code>run</code> method to be called in that separately executing
-     * thread.
-     * <p>
-     * The general contract of the method <code>run</code> is that it may
-     * take any action whatsoever.
-     *
-     * @see Thread#run()
+     * Gets ClientID
+     * @return ClientID
+     */
+    public int getClientID() {
+        return this._clientID;
+    }
+
+    /**
+     * Runs client thread
      */
     @Override
     public void run() {
+        try {
+            // Get client input
+            this._input = new BufferedReader(new InputStreamReader(this._socket.getInputStream()));
 
+            // Get client output
+            this._output = new ObjectOutputStream(this._socket.getOutputStream());
+
+            this._isConnected = true;
+
+        } catch (IOException ex) {
+            this._server.LogMessage("Connection to client ID# " + _clientID + " failed.");
+        } finally {
+            try {
+                this._socket.close();
+            } catch (IOException ex) {
+                this._server.LogMessage("Connection to client ID# " + _clientID + " closed.");
+            }
+        }
+    }
+
+    /**
+     * Sends a message to client
+     *
+     * @param message Message to send
+     * @throws ClientNotConnectedException Thrown if client is not connected
+     */
+    public void message(Message message) throws ClientNotConnectedException {
+        if(!this._isConnected){
+            throw new ClientNotConnectedException();
+        }
+        try {
+            this._output.writeObject(message);
+        } catch (Exception ex){
+
+        }
     }
 }
