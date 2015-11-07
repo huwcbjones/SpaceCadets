3ce63dc47009a8da194cb0279e35dc60d2116162
Huw Jones <huwcbjones@gmail.com>
2015-11-07 00:20:38 +0000

    Common CLI stuff

---
+++ Challenge 6/src/com/huwcbjones/chat/core/cli.java
@@ -0,0 +1,30 @@
+package com.huwcbjones.chat.core;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * {DESCRIPTION}
+ *
+ * @author Huw Jones
+ * @since 07/11/2015
+ */
+public class cli {
+
+    public static int getPortNumber(ArrayList<String> argList) throws Exception {
+        int port = 60666;
+        if (argList.contains("-p") || argList.contains("--port")) {
+            int index = (argList.contains("-p")) ? argList.indexOf("-p") : argList.indexOf("--port");
+            String intStr = argList.get(index + 1);
+            if (!intStr.matches("\\d")) {
+                throw new Exception("Invalid port number!");
+            }
+            port = Integer.parseInt(intStr);
+        }
+        return port;
+    }
+
+    public static URL getServer(ArrayList<String> argList) throws Exception {
+        return new URL("chat://" + argList.get(argList.size() - 1));
+    }
+}
