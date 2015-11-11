9cff8fcd137e6bb31f804608f6514a5259925832
Huw Jones <huwcbjones@gmail.com>
2015-11-11 01:38:15 +0000

    Started !help command

---
+++ Challenge 6/src/com/huwcbjones/chat/server/commands/help.java
@@ -0,0 +1,38 @@
+package com.huwcbjones.chat.server.commands;
+
+import com.huwcbjones.chat.core.Message;
+
+import java.util.ArrayList;
+
+/**
+ * {DESCRIPTION}
+ *
+ * @author Huw Jones
+ * @since 10/11/2015
+ */
+public class help implements Command {
+    @Override
+    public void getCommand() {
+
+    }
+
+    @Override
+    public String getHelp() {
+        return null;
+    }
+
+    @Override
+    public void addArguments(ArrayList<String> arguments) {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public Message getResult() {
+        return null;
+    }
+}
