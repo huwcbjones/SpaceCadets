ecc1965a0ed661cc9987a29c15d5e2124ac4b6b1
Huw Jones <huwcbjones@gmail.com>
2015-11-11 19:08:42 +0000

    Removed getResult and getCommand

--- Challenge 6/src/com/huwcbjones/chat/server/commands/Command.java
+++ Challenge 6/src/com/huwcbjones/chat/server/commands/Command.java
@@ -1,6 +1,8 @@
 package com.huwcbjones.chat.server.commands;
 
 import com.huwcbjones.chat.core.Message;
+import com.huwcbjones.chat.server.ChatServer;
+import com.huwcbjones.chat.server.ClientThread;
 
 import java.util.ArrayList;
 
@@ -11,13 +13,10 @@ import java.util.ArrayList;
  * @since 10/11/2015
  */
 public interface Command {
-    void getCommand();
 
     String getHelp();
 
-    void addArguments(ArrayList<String> arguments);
+    void addArguments(ArrayList<String> arguments) throws Exception;
 
-    void execute();
-
-    Message getResult();
+    void execute(ClientThread client, ChatServer server) throws Exception;
 }
