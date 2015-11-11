0366b434e524a55989dc3d4e3b5b1ee10e9f9503
Huw Jones <huwcbjones@gmail.com>
2015-11-11 19:08:58 +0000

    Finished help command (still needs list implementing)

--- Challenge 6/src/com/huwcbjones/chat/server/commands/help.java
+++ Challenge 6/src/com/huwcbjones/chat/server/commands/help.java
@@ -1,6 +1,8 @@
 package com.huwcbjones.chat.server.commands;
 
-import com.huwcbjones.chat.core.Message;
+import com.huwcbjones.chat.core.Frame;
+import com.huwcbjones.chat.server.ChatServer;
+import com.huwcbjones.chat.server.ClientThread;
 
 import java.util.ArrayList;
 
@@ -11,28 +13,41 @@ import java.util.ArrayList;
  * @since 10/11/2015
  */
 public class help implements Command {
-    @Override
-    public void getCommand() {
 
-    }
+    private ArrayList<String> _args;
 
     @Override
     public String getHelp() {
-        return null;
+        StringBuilder helpTxt = new StringBuilder();
+        helpTxt.append("help: Provides help with a command.\n");
+        helpTxt.append("USAGE: !help [COMMAND]\n");
+        helpTxt.append("PARAMS:\n");
+        helpTxt.append(" - list\t\tLists commands available.\n");
+        helpTxt.append(" - COMMAND\t\tCommand requiring help.\n");
+        return helpTxt.toString();
     }
 
     @Override
     public void addArguments(ArrayList<String> arguments) {
-
+        this._args = arguments;
     }
 
     @Override
-    public void execute() {
+    public void execute(ClientThread client, ChatServer server) {
+        if(_args.size() == 0){
+            client.write(new Frame(Frame.Type.HELP, this.getHelp()));
+            return;
+        }
+        if(_args.get(0).equals("list")){
 
-    }
-
-    @Override
-    public Message getResult() {
-        return null;
+            return;
+        }
+        try {
+            Command command = server.getCommand(_args.get(0));
+            client.write(new Frame(Frame.Type.HELP, command.getHelp()));
+        } catch (Exception ex){
+            String help = "Command not found: " + _args.get(0) + "\n";
+            client.write(new Frame(Frame.Type.HELP, help + this.getHelp()));
+        }
     }
 }
