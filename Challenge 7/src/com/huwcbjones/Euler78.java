873d31ba245fe0164daf4565d277f78548672077
Huw Jones <huwcbjones@gmail.com>
2015-11-13 00:00:33 +0000

    Added start of Euler Function/Pentagonal numbers

--- Challenge 7/src/com/huwcbjones/Euler78.java
+++ Challenge 7/src/com/huwcbjones/Euler78.java
@@ -1,5 +1,8 @@
 package com.huwcbjones;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Euler Challenge 78
  *
@@ -9,7 +12,31 @@ package com.huwcbjones;
 public class Euler78 {
 
     public static void main(String args[]){
-        Solver solver = new Solver();
-        solver.run(1);
+        ArrayList<String> argList = new ArrayList<>(Arrays.asList(args));
+        if (argList.contains("-h") || argList.contains("--help")) {
+            Euler78.help();
+        } else if (argList.contains("-v") || argList.contains("--version")) {
+            Euler78.version();
+        } else if (argList.contains("--stupid-mode")) {
+            Solver solver = new Solver();
+            solver.runStupidMode();
+        } else {
+            Solver solver = new Solver();
+            solver.run(1);
+        }
+    }
+
+    private static void help(){
+        System.out.println("Usage: Euler78 [OPTION]...");
+        System.out.println("Java chat server.\n");
+        System.out.println("Arguments:");
+        System.out.println("  -h, --help\t\tPrints this help message.");
+        System.out.println("      --stupid-mode\tRuns stupidly slowly, and east all your RAM! xD");
+        System.out.println("  -v, --version\t\tPrints version.");
+    }
+
+    private static void version() {
+        System.out.println("Euler78 0.1");
+        System.out.println("Written (poorly) by Huw Jones");
     }
 }
--- Challenge 7/src/com/huwcbjones/Solver.java
+++ Challenge 7/src/com/huwcbjones/Solver.java
@@ -15,6 +15,8 @@ public class Solver {
 
     private HashMap<Integer, HashSet<ArrayList<Integer>>> _counts = new HashMap<>();
 
+    private HashMap<Integer, Integer> _pValues = new HashMap<>();
+
     private long startTime;
     private long endTime;
 
@@ -25,22 +27,66 @@ public class Solver {
     public void run(int numberOfTimes) {
         this.startTime = System.nanoTime();
         for (int i = 0; i < numberOfTimes; i++) {
-            this._runStupidMode();
+            this._run();
         }
         this.endTime = System.nanoTime();
         System.out.println("Took " + (this.endTime - this.startTime) + "ns");
     }
 
+    private void _run() {
+        this._pValues.put(0, 1);
+        int pValue = 1;
+        int i = 1;
+        do {
+            System.out.print("Calculating: " + i);
+            pValue = _calculatePValue(i);
+            _pValues.put(i, pValue);
+            System.out.print(": " + pValue + "\n");
+            i++;
+        } while (pValue % 1000000 != 0);
+    }
+
+    private int _calculatePValue(int n) {
+        double pValueD = Math.pow(-1d, n + 1d);
+
+        int pIndex = new Double(n - (0.5 * n * 3 * n - 1)).intValue();
+
+
+        pValueD = pValueD * Math.pow(getPentagonal(n), getPValue(pIndex));// + this._pValues.get(bIndex));
+
+        int pValue = ((Double)pValueD).intValue();
+
+        for(int i =  (n - 1); i > 0; i--){
+            pValue += this._pValues.get(i);
+        }
+        return pValue;
+    }
+
+    private int getPValue(int n){
+        if(n < 0 ){
+            return 0;
+        }else{
+            return this._pValues.get(n);
+        }
+    }
+    public void runStupidMode() {
+        this.startTime = System.nanoTime();
+        this._runStupidMode();
+        this.endTime = System.nanoTime();
+        System.out.println("Took " + (this.endTime - this.startTime) + "ns");
+    }
+
+
     private void _runStupidMode() {
         HashSet<ArrayList<Integer>> set;
         int i = 1;
-        do{
+        do {
             System.out.print("Calculating: " + i);
             set = countN(i);
             System.out.print(": " + set.size() + "\n");
             this._counts.put(i, set);
             i++;
-        } while(set.size() % 1000000 != 0);
+        } while (set.size() % 1000000 != 0);
         System.out.println("end");
     }
 
@@ -50,7 +96,7 @@ public class Solver {
         for (int i = n; i != 0; i--) {
             // If _counts contains n, add already calculated sets to set
             if (_counts.containsKey(i)) {
-                for(ArrayList<Integer> intArray : this.getSet(i, n)){
+                for (ArrayList<Integer> intArray : this.getSet(i, n)) {
                     Collections.sort(intArray);
                     results.add(intArray);
                 }
@@ -90,4 +136,7 @@ public class Solver {
         return results;
     }
 
+    private int getPentagonal(int n){
+        return ((Double)((3 * Math.pow(n, 2) - 2) / 2)).intValue();
+    }
 }
