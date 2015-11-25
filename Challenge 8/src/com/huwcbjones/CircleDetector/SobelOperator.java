a64c301f2d73b38325a217927b409714b0a7c8b5
Huw Jones <huwcbjones@gmail.com>
2015-11-25 16:42:05 +0000

    Fixed Operator

--- Challenge 8/src/com/huwcbjones/CircleDetector/SobelOperator.java
+++ Challenge 8/src/com/huwcbjones/CircleDetector/SobelOperator.java
@@ -1,6 +1,5 @@
 package com.huwcbjones.CircleDetector;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 
 /**
@@ -53,62 +52,72 @@ public class SobelOperator {
     }
 
     public void process(){
-        for (int x = 0; x < image.getWidth(); x++) {
-            for (int y = 0; y < image.getHeight(); y++) {
+        for (int x = 0; x < img.length - 1; x++) {
+            for (int y = 0; y < img[0].length - 1; y++) {
                 Gx[x][y] = getGx(x, y);
                 Gy[x][y] = getGy(x, y);
-                G[x][y] = Double.valueOf(Math.sqrt(Gx[x][y] * Gx[x][y] + Gy[x][y] * Gy[x][y])).intValue();
+                G[x][y] = Double.valueOf(Math.sqrt((Gx[x][y] * Gx[x][y]) + (Gy[x][y] * Gy[x][y]))).intValue();
             }
         }
         int max = 0;
-        for (int x = 0; x < image.getWidth(); x++) {
-            for (int y = 0; y < image.getHeight(); y++) {
+        for (int x = 0; x < img.length; x++) {
+            for (int y = 0; y < img[0].length; y++) {
                 if(max < G[x][y]){
                     max = G[x][y];
                 }
             }
         }
-        double scaleFactor = max/255d;
-
-        for (int x = 0; x < image.getWidth(); x++) {
-            for (int y = 0; y < image.getHeight(); y++) {
-                int shade = Double.valueOf(G[x][y] / scaleFactor).intValue();
+        System.out.println(max);
+        double ratio = max / 255;
+        for (int x = 0; x < img.length; x++) {
+            for (int y = 0; y < img[0].length; y++) {
+                int shade = Double.valueOf(G[x][y] / ratio).intValue();
                 image.setRGB(x, y, 0xff000000 | shade << 16 | shade << 8 | shade);
             }
         }
     }
 
     private int getGx (int x, int y) {
-        int gx = 0;
-        // Gx Sobel Operator
-        // -1  0  1
-        // -2  0  2
-        // -1  0  1
-
-        for(int i : Mx){
-            gx += i * img[x][y];
-        }
-
-        return gx;
+        return getG(x, y, true);
     }
 
     private int getGy (int x, int y) {
-        int gy = 0;
-        // Gy Sobel Operator
-        // -1 -2 -1
-        //  0  0  0
-        //  1  2  1
+        return getG(x, y, false);
+    }
 
-        for(int i : My){
-            gy += i * img[x][y];;
+    private int getG(int x, int y, boolean isX) {
+        int sum = 0;
+        int i = 0;
+        int[] m = isX ? this.Mx : this.My;
+        System.out.print( x + ", " + y +":\n");
+        for (int My = y - 1; My <= y + 1; My++) {
+            System.out.print("|");
+            for (int Mx = x - 1; Mx <= x + 1; Mx++) {
+                int value = getImgCoord(Mx, My);
+                System.out.print(value + "|");
+                sum += m[i] * value;
+                i++;
+            }
+            System.out.print("\n");
         }
-        return gy;
+        System.out.print("\n");
+        return sum;
+    }
+
+    public int getImgCoord(int x, int y) {
+        if (
+                (x < 0 || y < 0)
+                        || (x >= img.length || y >= img[0].length)
+                ) {
+            return 0;
+        }
+        return img[x][y];
     }
 
     public int getCoord (int x, int y) {
         if (
-                ( x <= 0 || y <= 0 )
-                        || ( x > Gx.length || y > Gy.length )
+                (x < 0 || y < 0)
+                        || (x >= img.length || y >= img[0].length)
                 ) {
             return 0;
         }
